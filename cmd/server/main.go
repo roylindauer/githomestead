308bcd73c664e119566f47777ace97aab7b6c13a
Roy Lindauer <1851879+roylindauer@users.noreply.github.com>
2025-01-13 11:53:03 -0800

    Server now read config.yml for app config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,18 +3,41 @@ package main
 import (
 	"gitapi/internal/gitserver"
 	"gitapi/internal/transport"
-	"github.com/joho/godotenv"
+	"github.com/go-yaml/yaml"
 	"log"
 	"os"
 )
 
+type Config struct {
+	Port     string `yaml:"port"`
+	Endpoint string `yaml:"endpoint"`
+}
+
 func main() {
-	err := godotenv.Load()
+	f, err := os.Open("config.yml")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println(err)
+		panic(err)
 	}
 
-	svc := gitserver.NewService(os.Getenv("GIT_SERVER_ENDPOINT"))
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+	}(f)
+
+	var cfg Config
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	svc := gitserver.NewService(cfg.Endpoint)
 
 	server := transport.NewServer(*svc)
 	if err := server.Serve(); err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module gitapi
 
 go 1.23
 
-require github.com/joho/godotenv v1.5.1
+require github.com/go-yaml/yaml v2.1.0+incompatible
+
+require (
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
